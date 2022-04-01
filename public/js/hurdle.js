1d397aa5ffe47fb36fd8d3f24dbc33f3fda6308b
Sunny KaPatel <psunny1411@gmail.com>
2022-04-01 04:25:20 -0600

    Added the basic structure

---
+++ public/js/hurdle.js
@@ -0,0 +1,38 @@
+const row1 = document.querySelector('#row1');
+const cell00 = document.querySelector('#cell00');
+const cell01 = document.querySelector('#cell01');
+const cell02 = document.querySelector('#cell02');
+const cell03 = document.querySelector('#cell03');
+const cell04 = document.querySelector('#cell04');
+
+const row2 = document.querySelector('#row2');
+const cell10 = document.querySelector('#cell10');
+const cell11 = document.querySelector('#cell11');
+const cell12 = document.querySelector('#cell12');
+const cell13 = document.querySelector('#cell13');
+const cell14 = document.querySelector('#cell14');
+
+const row3 = document.querySelector('#row3');
+const cell20 = document.querySelector('#cell20');
+const cell21 = document.querySelector('#cell21');
+const cell22 = document.querySelector('#cell22');
+const cell23 = document.querySelector('#cell23');
+const cell24 = document.querySelector('#cell24');
+
+const row4 = document.querySelector('#row4');
+const cell30 = document.querySelector('#cell30');
+const cell31 = document.querySelector('#cell31');
+const cell32 = document.querySelector('#cell32');
+const cell33 = document.querySelector('#cell33');
+const cell34 = document.querySelector('#cell34');
+
+const row5 = document.querySelector('#row5');
+const cell40 = document.querySelector('#cell40');
+const cell41 = document.querySelector('#cell41');
+const cell42 = document.querySelector('#cell42');
+const cell43 = document.querySelector('#cell43');
+const cell44 = document.querySelector('#cell44');
+
+window.onload = function () {
+    cell00.focus();
+}   
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public/views'));
 
+app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public/css/'));
+
 app.get('/', (req, res) => {
     res.render('hurdle');
 });
